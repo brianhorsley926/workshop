06d9718919a7e314957dc95126801a341c0098ac
brianhorsley926 <brianhorsley926@gmail.com>
2024-08-11 17:00:55 -0500

    Updated some score logic to account for the ace multi-value. Fixed bugs where the method required explicit returns

--- blackjack_1.rb
+++ blackjack_1.rb
@@ -48,6 +48,7 @@ def deal_a_hand()
     dealer = []
     player = []
     for card in 1..deal_number_of_cards
+        # Deals the initial hands for the player and Dealer
         player << deal_a_card()
         # binding.pry # DEBUGGING
         puts "#{$player_name} has the #{player[card - 1][0]} of #{player[card - 1][1]}"
@@ -59,7 +60,7 @@ def deal_a_hand()
     puts "\nIt's your turn #{$player_name}.\n"
 
     $turn_number += 1
-    return dealer, player
+    return dealer, player # Why didn't this work without return?
 end
 
 # Uses the Array#pop method to deal a card and remove it from the Deck
@@ -72,26 +73,71 @@ def remaining_cards()
     puts $list_of_cards
 end 
 
-def check_score(card_value)
-    case card_value
-    when 'jack'
-        10
-    when 'queen'
-        10
-    when 'king'
-        10
-    when 'ace'
-        11
-    else
-        card_value
+# Returns back the int value of a card
+# I am missing the logic for handling the ace having two values (1, 11)
+def check_score(hand)
+    score = 0
+    number_of_aces = 0
+
+    hand.each do |card|
+        card_value = card[0]
+
+        case card_value
+        when 'jack'
+            score += 10
+        when 'queen'
+            score += 10
+        when 'king'
+            score += 10
+        when 'ace'
+            score += 11
+            number_of_aces += 1
+        else
+            score += card_value.to_i # This was original making numbered cards string types
+        end
     end
+
+    while score > 21 && number_of_aces > 0
+        score -= 10 # Adjust one ace from 11 to 1
+        number_of_aces -= 1
+    end
+
+    return score # Without the return this didn't work??
 end
 
+# Check if the player/dealer score is a bust
 def bust?(score)
     score > 21
 end
 
+# Populates the Deck of cards
+def reset_deck(cards)
+    cards.each do |suit, value|
+        value.each do |v|
+            $list_of_cards << [v,suit]
+        end
+    end
+end
+
+# # Assigns 1 or 11 to an ace depending on the current score
+# def check_aces(score, hand)
+#     # This count method counts the number of elements in the cards array that are an 'ace'
+#     aces_in_hand = hand.count{|card| card[0] == 'ace'}
+#     while aces_in_hand > 0 && score > 21
+#         # Make the ace value 1 
+#         score -= 10
+#         # Now that the value is adjusted take away that ace from consideration
+#         aces_in_hand -= 1
+#     end
+#     score 
+# end
+
+# Game logic
 def game_of_blackjack()
+    # Shuffle the deck
+    puts "The deck has been shuffled.\n"
+    shuffle()   
+
     # Initialize the player's scores
     player_score = 0
     dealer_score = 0
@@ -107,10 +153,14 @@ def game_of_blackjack()
         initial_hands = deal_a_hand()
 
         # binding.pry # DEBUGGING
-        player_score = check_score(initial_hands[1][0][0]) + check_score(initial_hands[1][1][0])
-        dealer_score = check_score(initial_hands[0][0][0]) + check_score(initial_hands[0][1][0])
+        # Initial hand returns multi-dimensional array - check for any aces that cause a bust for dealer/player
+        player_score = check_score(initial_hands[1])
+        # player_score = check_aces(player_score, initial_hands[1])
+
+        dealer_score = check_score(initial_hands[0])
+        # dealer_score = check_aces(dealer_score, initial_hands[0])
         
-        # Check if anyone hit blackjack from the initial hand
+        # Check if anyone hit blackjack from the initial hand, prioritizing the player
         if player_score == 21
             puts "#{$player_name} hits blackjack! You win!"
             return
@@ -123,6 +173,7 @@ def game_of_blackjack()
         end
 
         # Player's turn
+        puts "It's #{$player_name}'s turn."
         while true
             puts 'Will you hit or stand?'
             player_choice = gets.chomp
@@ -132,10 +183,14 @@ def game_of_blackjack()
                 puts "Dealer needs to beat #{player_score}"
                 $player_turn = false
                 break
+            # If player hits, continue until they bust or stand
             elsif player_choice == 'hit'
                 next_card = deal_a_card()
                 puts "#{next_card[0]} of #{next_card[1]}\n"
-                player_score += check_score(next_card[0])
+
+                initial_hands[1] << next_card
+                player_score = check_score(initial_hands[1])
+                # player_score = check_aces(player_score, initial_hands[1])
                 if bust?(player_score)
                     puts "You bust with a score of #{player_score}. House wins!"
                     return
@@ -147,16 +202,20 @@ def game_of_blackjack()
         end
 
         # Dealer's turn
-        while dealer_score <= 16
+        puts "It's the #{$dealer_name}'s turn."
+
+        # Dealer hits unless score is 17>
+        while dealer_score < 17
             next_card = deal_a_card()
             puts "#{next_card[0]} of #{next_card[1]}\n"
-            dealer_score += check_score(next_card[0])
+
+            initial_hands[0] << next_card
+            dealer_score = check_score(initial_hands[0])
 
             if bust?(dealer_score)
                 puts "#{$dealer_name} busts with a score of #{dealer_score}. #{$player_name} wins!"
                 return
             end
-
             puts "#{$dealer_name} now has #{dealer_score}"
         end
 
@@ -180,16 +239,8 @@ cards = {
     'clubs' => [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'],
     'diamonds' => [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace']
 }
-# Populates the Deck of cards
-cards.each do |suit, value|
-    value.each do |v|
-        $list_of_cards << [v,suit]
-    end
-end
 
-# Shuffle the deck
-puts "The deck has been shuffled.\n"
-shuffle()
+reset_deck(cards)
 
 # Start the game
 game_of_blackjack()
@@ -199,6 +250,7 @@ puts "Play again? (Y/N)"
 play_again = gets.chomp 
 
 while play_again == 'Y'
+    reset_deck(cards)
     game_of_blackjack()
     puts "Play again? (Y/N)"
     play_again = gets.chomp 
